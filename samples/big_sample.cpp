7f3fc2aee32bd53c25b68e4799b251d62d12d527
Anton Kovalev <camotank12345@gmail.com>
2023-03-09 05:38:35 +0300

    And another verify

--- samples/big_sample.cpp
+++ samples/big_sample.cpp
@@ -1,19 +1,23 @@
 #include <grr/grr.hpp>
 #include <iostream>
 
+using int_vector = std::vector<int>;
+
 struct my_struct
 {
 	int a;
 	std::uint64_t b;
 	grr::string s1;
 	grr::string s2;
+	int_vector memory;
 };
 
 int main()
 {
-	const my_struct instance = { 1, 2, "hello reflection", "under reflection" };
+	const my_struct instance = { 1, 2, "hello reflection", "under reflection", { 1, 2, 3, 4} };
 
 	grr::context context = grr::make_context();
+	grr::add_type<int_vector>(context);
 	grr::add_type<my_struct>(context);
 	grr::rename<my_struct>(context, "SUPER PUPER STRUCTURE");
 
@@ -31,8 +35,16 @@ int main()
 			std::cout << name << ": " << std::to_string(field) << std::endl;
 		} else if constexpr (std::is_same_v<T, grr::string>) {
 			std::cout << name << ": " << field << std::endl;
-		} else {
-			std::cout << name << ": " << "unknown memory" << std::endl;
+		} else if constexpr (std::is_same_v<T, grr::const_ptr_pair>) {
+			constexpr grr::type_id vector_typeid = grr::obtain_id<int_vector>();
+			if (vector_typeid == field.second.second) {
+				const int_vector* my_vector = reinterpret_cast<const int_vector*>(field.second.first);
+				std::cout << name << ": " << "vector ( ";
+				for (const auto& elem : *my_vector) {
+					std::cout << std::to_string(elem) << " ";
+				}
+				std::cout << ")" << std::endl;
+			}
 		}
 	});
 
