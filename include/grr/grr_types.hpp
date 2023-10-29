6884ff31ac085451560704c2b304df8e42e1c03d
Anton Kovalev <camotank12345@gmail.com>
2023-10-29 09:01:12 +0100

    Replaced license in headers

--- include/grr/grr_types.hpp
+++ include/grr/grr_types.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************************
 * Copyright (C) Anton Kovalev (vertver), 2023. All rights reserved.
 * GRR - "Games Require Reflection", library for integrating reflection into games
-* MIT License
+* Boost Software License, Version 1.0
 ***************************************************************************************/
 #ifndef GRR_TYPES_HPP_INCLUDED
 #define GRR_TYPES_HPP_INCLUDED
